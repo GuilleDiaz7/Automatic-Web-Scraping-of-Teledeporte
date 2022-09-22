352b5374a830bb7d5d6c9cfd7d6d6b470570ba0d
Guillermo Diaz <guillermo.dgr.7@gmail.com>
2022-09-22 11:21:08 +0200

    Preparing cleaning script.

---
+++ data_cleaning.R
@@ -0,0 +1,35 @@
+library(dplyr)
+library(stringr)
+
+df <- read.csv("https://raw.githubusercontent.com/GuilleDiaz7/Automatic-Web-Scraping-of-Teledeporte/main/data/teledeporte.csv",
+               fileEncoding = "UTF-8")
+
+df %>% head()
+df$gender <- NA
+
+df <- df %>% 
+  mutate(
+    sport = case_when(
+      str_detect(sp_title, "WTA") ~ "Tennis",
+      str_detect(sp_title, "ATP") ~ "Tennis",
+      str_detect(sp_title, regex("Estudio estadio", ignore_case = T)) ~ "Football",
+      str_detect(sp_title, "UEFA") ~ "Football",
+      str_detect(sp_title, "motociclismo") ~ "Motorbikes",
+      str_detect(sp_title, regex("rallycross", ignore_case = T)) ~ "Cars",
+      str_detect(sp_title, regex("basket", ignore_case = T)) ~ "Basket"
+    ),
+    category = case_when(
+      str_detect(sp_title, "FIA") ~ "Motor sports",
+      str_detect(sp_title, regex("motociclismo", ignore_case = T)) ~ "Motor sports",
+      str_detect(sp_title, regex("wta|atp", ignore_case = T)) ~ "Racket sports",
+      str_detect(sp_title, regex("uefa|fifa", ignore_case = T)) ~ "Team sports"
+    ),
+    gender = case_when(
+      str_detect(sp_title, "WTA") ~ "Female",
+      str_detect(sp_title, "ATP") ~ "Male"
+    )
+  )
+
+View(df)
+
+# a ver deberia tener: tournament, program_type, sports_category (team, motor, racket, etc), sport, subsport, gender, broadcast_type (live, repeated)
